fe3aa6d7b003196244760d0a0c747b055718cb8c
sebug <sebastian.gfeller@gmail.com>
2020-02-10 20:09:35 +0100

    Class

--- src/shell.js
+++ src/shell.js
@@ -2,12 +2,16 @@ import '../libs/knockout.js';
 import './edit-node.js';
 import Node from './models/node.js';
 
-ko.components.register('shell', {
-    viewModel: function() {
+class ShellViewModel {
+    constructor() {
 	this.message = ko.observable("Hello from shell");
 	this.node = ko.observable(new Node());
-	this.node().name('from class instance');
-    },
+	this.node().name('from class instance of class');
+    }
+}
+
+ko.components.register('shell', {
+    viewModel: ShellViewModel,
     template: '<main>' +
 	'<h1><span data-bind="text: message"></span></h1>' +
 	'<edit-node params="node: node"></edit-node>' +
